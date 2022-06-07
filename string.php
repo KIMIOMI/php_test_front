37d6dd600205eb03a601a03c76505694c1348304
kimiomi <plusee@naver.com>
2022-06-08 00:38:51 +0900

    initial

---
+++ string.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>String & String Operator</h1>
+    <?php
+    echo "hello \"w\"ord"
+      ?>
+    <h2>concatenation Operator</h2>
+    <?php
+    echo "Hello "."world";
+     ?>
+     <h2>String length</h2>
+     <?php
+     $str = 'abcdef';
+     echo strlen($str);
+      ?>
+  </body>
+</html>
---
+++ variable.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "memememememem";
+    echo "Lorem ipsum dolor sit amet,".$name." consectetur adipisicing elit,".$name." sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.".$name." Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     ?>
+
+  </body>
+</html>
