c52937cfe7d2c6314af9e85feff8c628d9168d92
kimiomi <plusee@naver.com>
2022-06-08 01:23:55 +0900

    initial atom

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-	echo "hi"
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<h1>WEB</h1>
+		<ol>
+			<li><a href="index.php?id=HTML">HTML</a> </li>
+			<li><a href="index.php?id=CSS">CSS</a></li>
+			<li><a href="index.php?id=JavaScript">JavaScript</a></li>
+		</ol>
+		<?php
+			echo $_GET['id'];
+		?>
+	</body>
+</html>
